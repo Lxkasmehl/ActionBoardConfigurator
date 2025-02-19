ec422c1bd2a0fa253bd9746197480bfa215f15e9
Lxkasmehl <lukasmehl007@gmail.com>
2025-02-19 09:18:57 -0600

    Refactor App and EntitySection components to integrate @xyflow/react for enhanced node management; remove @dnd-kit/core dependency and update drag-and-drop logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,48 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useCallback } from 'react';
+import { useSelector } from 'react-redux';
 import { IconButton } from '@mui/joy';
-import { closestCorners, DndContext } from '@dnd-kit/core';
-
-import { deleteID, deleteRawFormDataForId } from './redux/entitiesSlice.js';
 import useFetchEntities from './hooks/useFetchEntities.js';
 import EntitySection from './components/EntitySection.jsx';
 
 import AddIcon from '@mui/icons-material/Add';
+import {
+  Background,
+  Controls,
+  ReactFlow,
+  useEdgesState,
+  useNodesState,
+  addEdge,
+} from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+
+const nodeTypes = {
+  EntitySection: EntitySection,
+};
 
 export default function App() {
   const loading = useFetchEntities();
-  const [sections, setSections] = useState([
+  const config = useSelector((state) => state.entities.config);
+
+  const [nodes, setNodes, onNodesChange] = useNodesState([
     {
-      id: 0,
+      id: '0',
       position: {
-        x: window.innerWidth / 2 - 320,
+        x: window.innerWidth / 2 - 350,
         y: window.innerHeight / 2 - 55,
       },
+      data: {},
+      type: 'EntitySection',
     },
   ]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
-  const dispatch = useDispatch();
-  const config = useSelector((state) => state.entities.config);
+  const onConnect = useCallback(
+    (connection) => {
+      const edge = { ...connection, id: `${edges.length} + 1` };
+      setEdges((prevEdges) => addEdge(edge, prevEdges));
+    },
+    [edges.length, setEdges],
+  );
 
   const addSection = () => {
     const windowWidth = window.innerWidth;
@@ -31,7 +51,7 @@ export default function App() {
     let newX = windowWidth / 2 - 320;
     let newY = windowHeight / 2 - 55;
 
-    const occupiedPositions = sections.map((s) => s.position);
+    const occupiedPositions = nodes.map((s) => s.position);
     while (
       occupiedPositions.some(
         (pos) => Math.abs(pos.x - newX) < 700 && Math.abs(pos.y - newY) < 150,
@@ -41,47 +61,21 @@ export default function App() {
       newY += 20;
     }
 
-    const newId = sections.length
-      ? Math.max(...sections.map((s) => s.id)) + 1
-      : 0;
-    setSections((prev) => [
+    const newId = (
+      nodes.length ? Math.max(...nodes.map((s) => Number(s.id))) + 1 : 0
+    ).toString();
+
+    setNodes((prev) => [
       ...prev,
-      { id: newId, position: { x: newX, y: newY } },
+      {
+        id: newId,
+        position: { x: newX, y: newY },
+        data: {},
+        type: 'EntitySection',
+      },
     ]);
-  };
 
-  const removeSection = (id) => {
-    setSections((prev) => prev.filter((section) => section.id !== id));
-    dispatch(deleteID(id));
-    dispatch(deleteRawFormDataForId({ id }));
     console.log(config);
-  };
-
-  const handleDragEnd = (event) => {
-    const { active, delta } = event;
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-
-    setSections((prevSections) =>
-      prevSections.map((section) => {
-        if (section.id !== active.id) return section;
-
-        const newX = Math.min(
-          Math.max(section.position.x + delta.x, 0),
-          windowWidth - 700,
-        );
-
-        const newY = Math.min(
-          Math.max(section.position.y + delta.y, 0),
-          windowHeight - 150,
-        );
-
-        return {
-          ...section,
-          position: { x: newX, y: newY },
-        };
-      }),
-    );
   };
 
   if (loading) {
@@ -92,47 +86,36 @@ export default function App() {
     );
   }
 
-  // TODO: React Flow or React XArrows
-
   return (
-    <DndContext onDragEnd={handleDragEnd} collisionDetection={closestCorners}>
-      <div>
-        <div className='w-full flex flex-col items-center'>
-          {sections.map((section) => (
-            <div
-              key={section.id}
-              className='flex flex-col items-center mt-5'
-              style={{
-                position: 'absolute',
-                left: section.position.x,
-                top: section.position.y,
-              }}
-            >
-              <EntitySection
-                key={section.id}
-                id={section.id}
-                onRemove={() => removeSection(section.id)}
-              />
-            </div>
-          ))}
-        </div>
+    <div className='w-full h-screen'>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        proOptions={{ hideAttribution: true }}
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
 
-        <IconButton
-          onClick={addSection}
-          variant='solid'
-          aria-label='Add new entity section'
-          sx={{
-            position: 'fixed',
-            bottom: '40px',
-            right: '40px',
-            width: '64px',
-            height: '64px',
-            borderRadius: '50%',
-          }}
-        >
-          <AddIcon sx={{ fontSize: 32 }} />
-        </IconButton>
-      </div>
-    </DndContext>
+      <IconButton
+        onClick={addSection}
+        variant='solid'
+        aria-label='Add new entity section'
+        sx={{
+          position: 'fixed',
+          bottom: '40px',
+          right: '40px',
+          width: '64px',
+          height: '64px',
+          borderRadius: '50%',
+        }}
+      >
+        <AddIcon sx={{ fontSize: 32 }} />
+      </IconButton>
+    </div>
   );
 }
--- src/components/EntitySection.jsx
+++ src/components/EntitySection.jsx
@@ -9,15 +9,15 @@ import {
   deletePropertySelection,
   setPropertyOptions,
   deleteRawFormDataForId,
+  deleteID,
 } from '../redux/entitiesSlice';
 import PropTypes from 'prop-types';
 import { Button, Card, IconButton, Tooltip } from '@mui/joy';
 import FilterModal from './FilterModal';
-import { useDraggable } from '@dnd-kit/core';
-import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import RemoveIcon from '@mui/icons-material/Remove';
+import { useReactFlow } from '@xyflow/react';
 
-export default function EntitySection({ id, onRemove }) {
+export default function EntitySection({ id }) {
   const dispatch = useDispatch();
   const filteredEntities = useSelector(
     (state) => state.entities.filteredEntities,
@@ -42,13 +42,7 @@ export default function EntitySection({ id, onRemove }) {
   const centerDropdownRef = useRef(null);
   const rightDropdownRef = useRef(null);
 
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({ id });
-
-  const style = transform
-    ? {
-        transform: `translate(${transform.x}px, ${transform.y}px)`,
-      }
-    : undefined;
+  const { setNodes } = useReactFlow();
 
   const handleEntityChange = (entityName) => {
     if (selectedEntity) {
@@ -94,87 +88,77 @@ export default function EntitySection({ id, onRemove }) {
     console.log(config);
   };
 
+  const handleRemove = () => {
+    setNodes((prevNodes) => prevNodes.filter((node) => node.id !== id));
+    dispatch(deleteID(id));
+    dispatch(deleteRawFormDataForId({ id }));
+    console.log(config);
+  };
+
   return (
     // className='flex items-center justify-around px-6 py-8 bg-gray-800 lg:w-[50em] w-[90%] text-white rounded-lg shadow-lg'
     <>
-      <div style={style}>
-        <Card
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            width: '40em',
-          }}
-        >
-          <Dropdown
-            id='dropdown-left'
-            options={filteredEntities.map((e) => ({
-              value: e.name,
-              label: e['sap:label'] || e.name,
-            }))}
-            defaultValue='Select an entity'
-            onChange={handleEntityChange}
-          />
-          <div className='flex items-center'>
-            <Button
-              color='neutral'
-              variant='outlined'
-              onClick={() => setFilterModalOpen(true)}
-              disabled={!selectedEntity}
-            >
-              {!rawFormData[id] ? 'Add Filter' : 'Edit Filter'}
-            </Button>
-          </div>
+      <Card
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          width: '40em',
+        }}
+      >
+        <Dropdown
+          id='dropdown-left'
+          options={filteredEntities.map((e) => ({
+            value: e.name,
+            label: e['sap:label'] || e.name,
+          }))}
+          defaultValue='Select an entity'
+          onChange={handleEntityChange}
+        />
+        <div className='flex items-center'>
+          <Button
+            color='neutral'
+            variant='outlined'
+            onClick={() => setFilterModalOpen(true)}
+            disabled={!selectedEntity}
+          >
+            {!rawFormData[id] ? 'Add Filter' : 'Edit Filter'}
+          </Button>
+        </div>
 
-          <Tooltip
-            title='Select all properties you want to display'
-            placement='top'
-            variant='solid'
-          >
-            <span>
-              <Dropdown
-                id='dropdown-right'
-                options={propertyOptions.map((p) => ({
-                  value: p.Name,
-                  label: p['sap:label'] || p.Name,
-                }))}
-                defaultValue={'Select a property'}
-                onChange={handleSelectedPropertyChange}
-                ref={rightDropdownRef}
-                multiple={true}
-              />
-            </span>
-          </Tooltip>
-          <IconButton
-            {...attributes}
-            {...listeners}
-            ref={setNodeRef}
-            sx={{
-              ml: -4,
-              mr: -2,
-              cursor: ['-webkit-grab', '-moz-grab', 'grab'],
-              '&:active': {
-                cursor: ['-webkit-grabbing', '-moz-grabbing', 'grabbing'],
-              },
-            }}
-          >
-            <DragIndicatorIcon />
-          </IconButton>
-        </Card>
-        <IconButton
-          onClick={onRemove}
-          variant='outlined'
-          color='danger'
-          sx={{
-            position: 'absolute',
-            left: '-60px',
-            top: 'calc(50% - 18px)',
-          }}
+        <Tooltip
+          title='Select all properties you want to display'
+          placement='top'
+          variant='solid'
         >
-          <RemoveIcon />
-        </IconButton>
-      </div>
+          <span>
+            <Dropdown
+              id='dropdown-right'
+              options={propertyOptions.map((p) => ({
+                value: p.Name,
+                label: p['sap:label'] || p.Name,
+              }))}
+              defaultValue={'Select a property'}
+              onChange={handleSelectedPropertyChange}
+              ref={rightDropdownRef}
+              multiple={true}
+            />
+          </span>
+        </Tooltip>
+      </Card>
+      <IconButton
+        onClick={handleRemove}
+        variant='outlined'
+        color='danger'
+        sx={{
+          position: 'absolute',
+          left: '-60px',
+          top: 'calc(50% - 18px)',
+        }}
+      >
+        <RemoveIcon />
+      </IconButton>
       <FilterModal
         open={filterModalOpen}
         onClose={() => setFilterModalOpen(false)}
@@ -187,5 +171,4 @@ export default function EntitySection({ id, onRemove }) {
 
 EntitySection.propTypes = {
   id: PropTypes.number.isRequired,
-  onRemove: PropTypes.func.isRequired,
 };
