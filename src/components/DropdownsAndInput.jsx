bdde298fe367550e9f0073b4a9619d8f57fb2a60
Lxkasmehl <lukasmehl007@gmail.com>
2025-03-06 17:18:32 -0600

    refactor: Extract state management logic from DropdownsAndInput to custom hook

--- src/components/DropdownsAndInput.jsx
+++ src/components/DropdownsAndInput.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
 import { Autocomplete, Select, Option, Card, Typography } from '@mui/joy';
-import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import {
   OPERATOR_OPTIONS,
@@ -8,166 +6,20 @@ import {
   SELECT_STYLES,
 } from './dropdownsAndInput.constants';
 import PropertyTypeInput from './PropertyTypeInput';
-import usePropertyOptions from '../hooks/usePropertyOptions';
-import {
-  findMatchingEntity,
-  getNavigationProperties,
-} from '../utils/navigationUtils';
+import useDropdownsAndInputState from '../hooks/useDropdownsAndInputState';
 
 export default function DropdownsAndInput({
   propertyOptionsId,
   fieldIdentifierId,
   ...props
 }) {
-  const formData = useSelector((state) => state.entities.formData);
-  const storedMatchingEntityObjects = useSelector(
-    (state) => state.entities.matchingEntityObjects,
-  );
-
-  const allEntities = useSelector((state) => state.entities.allEntities);
-  const associationSets = useSelector(
-    (state) => state.entities.associationSets,
-  );
-  const filteredEntities = useSelector(
-    (state) => state.entities.filteredEntities,
-  );
-  const selectedEntities = useSelector(
-    (state) => state.entities.selectedEntities,
-  );
-  const selectedEntity = selectedEntities[propertyOptionsId];
-
-  const { combinedOptions, propertyOptions } =
-    usePropertyOptions(propertyOptionsId);
-
-  const [property, setProperty] = useState(() => {
-    const storedPropertyName =
-      formData[propertyOptionsId]?.[`property_${fieldIdentifierId}`];
-
-    return storedPropertyName
-      ? propertyOptions.find((prop) => prop.Name === storedPropertyName) || null
-      : null;
-  });
-
-  const [operator, setOperator] = useState(
-    formData[propertyOptionsId]?.[`operator_${fieldIdentifierId}`] ?? '',
-  );
-
-  const [value, setValue] = useState(
-    formData[propertyOptionsId]?.[`value_${fieldIdentifierId}`] ?? '',
-  );
-
-  const [matchingEntityObjectState, setMatchingEntityObjectState] = useState(
-    () => {
-      const storedState =
-        storedMatchingEntityObjects[propertyOptionsId]?.[fieldIdentifierId];
-      if (storedState) {
-        return storedState;
-      }
-      return null;
-    },
-  );
-
-  const [path, setPath] = useState(
-    formData[propertyOptionsId]?.[`fullPath_${fieldIdentifierId}`] ?? '',
-  );
-
-  const [autocompleteKey, setAutocompleteKey] = useState(0);
-
-  const [partialPath, setPartialPath] = useState(() => {
-    const initialPath =
-      formData[propertyOptionsId]?.[`fullPath_${fieldIdentifierId}`] ?? '';
-    const lastSlashIndex = initialPath.lastIndexOf('/');
-    return lastSlashIndex >= 0 ? initialPath.substring(0, lastSlashIndex) : '';
-  });
-
-  const handleValueChange = (e) => {
-    setValue(typeof e === 'object' && e.target ? e.target.value : e);
-  };
-
-  const handlePropertyChange = (e, newValue) => {
-    if (!newValue) return;
-
-    const newValueName = newValue.Name;
-
-    const currentEntity =
-      matchingEntityObjectState?.matchingEntity ||
-      filteredEntities.find((e) => e.name === selectedEntity);
-
-    const navigationProperties = getNavigationProperties(currentEntity);
-
-    const isNavigationProperty = navigationProperties.some((np) =>
-      np.Name.endsWith('Nav')
-        ? np.Name.slice(0, -3) === newValueName
-        : np.Name === newValueName,
-    );
-
-    if (!isNavigationProperty) {
-      setProperty(newValue);
-      const pathParts = path.split('/');
-
-      let relevantNavigationProperties = navigationProperties;
-      if (pathParts.length > 0) {
-        const currentEntity = filteredEntities.find(
-          (e) => e.name === selectedEntity,
-        );
-        const { matchingEntity: newCurrentEntity } =
-          findMatchingEntity({
-            propertyName: pathParts[pathParts.length - 1],
-            navigationProperties: currentEntity.properties.navigationProperties,
-            associationSets,
-            allEntities,
-          }) || {};
-
-        if (newCurrentEntity) {
-          relevantNavigationProperties =
-            newCurrentEntity.properties.navigationProperties;
-        }
-      }
-
-      const lastPartIsNavigation = relevantNavigationProperties.some((np) =>
-        np.Name.endsWith('Nav')
-          ? np.Name.slice(0, -3) === pathParts[pathParts.length - 1]
-          : np.Name === pathParts[pathParts.length - 1],
-      );
-
-      const newPath = matchingEntityObjectState
-        ? pathParts.length > 0
-          ? lastPartIsNavigation
-            ? `${path}/${newValueName}`
-            : [...pathParts.slice(0, -1), newValueName].join('/')
-          : newValueName
-        : newValueName;
-
-      setPath(newPath);
-      setMatchingEntityObjectState({
-        ...matchingEntityObjectState,
-        path: newPath,
-      });
-      setAutocompleteKey((prev) => prev + 1);
-      return;
-    }
-
-    const newPath = matchingEntityObjectState
-      ? `${partialPath !== '' ? `${partialPath}/` : ''}${newValueName}`
-      : newValueName;
-    setPath(newPath);
-    setPartialPath(newPath);
-
-    const { matchingEntity } =
-      findMatchingEntity({
-        propertyName: newValueName,
-        navigationProperties,
-        associationSets,
-        allEntities,
-      }) || {};
-
-    const matchingEntityObject = matchingEntity
-      ? { path: newPath, matchingEntity }
-      : { path: newPath, matchingEntity: {} };
-
-    setMatchingEntityObjectState(matchingEntityObject);
-    setAutocompleteKey((prev) => prev + 1);
-  };
+  const {
+    state,
+    handleValueChange,
+    handlePropertyChange,
+    combinedOptions,
+    dispatch,
+  } = useDropdownsAndInputState(propertyOptionsId, fieldIdentifierId);
 
   const commonAutocompleteProps = {
     placeholder: 'Property',
@@ -183,8 +35,8 @@ export default function DropdownsAndInput({
         type='hidden'
         name={`matchingEntityObject_${fieldIdentifierId}`}
         value={
-          matchingEntityObjectState
-            ? JSON.stringify(matchingEntityObjectState)
+          state.matchingEntityObjectState
+            ? JSON.stringify(state.matchingEntityObjectState)
             : ''
         }
       />
@@ -192,14 +44,14 @@ export default function DropdownsAndInput({
         type='hidden'
         name={`fullPath_${fieldIdentifierId}`}
         defaultValue={
-          matchingEntityObjectState
-            ? matchingEntityObjectState.path
-            : property?.Name || ''
+          state.matchingEntityObjectState
+            ? state.matchingEntityObjectState.path
+            : state.property?.Name || ''
         }
       />
 
-      {Object.keys(matchingEntityObjectState?.matchingEntity || {}).length >
-        0 && (
+      {Object.keys(state.matchingEntityObjectState?.matchingEntity || {})
+        .length > 0 && (
         <Card
           sx={{
             height: '36px',
@@ -210,24 +62,24 @@ export default function DropdownsAndInput({
             borderBottomRightRadius: 0,
           }}
         >
-          <Typography>{`${partialPath}/`}</Typography>
+          <Typography>{`${state.partialPath}/`}</Typography>
         </Card>
       )}
 
       <Autocomplete
         {...commonAutocompleteProps}
-        key={autocompleteKey}
+        key={state.autocompleteKey}
         options={
-          Object.keys(matchingEntityObjectState?.matchingEntity || {}).length >
-          0
+          Object.keys(state.matchingEntityObjectState?.matchingEntity || {})
+            .length > 0
             ? (() => {
                 const options = [
                   ...Object.values(
-                    matchingEntityObjectState.matchingEntity?.properties
+                    state.matchingEntityObjectState.matchingEntity?.properties
                       ?.properties || {},
                   ).flat(),
                   ...Object.values(
-                    matchingEntityObjectState.matchingEntity?.properties
+                    state.matchingEntityObjectState.matchingEntity?.properties
                       ?.navigationProperties || {},
                   ).flat(),
                 ].sort((a, b) => a.Name.localeCompare(b.Name));
@@ -241,18 +93,20 @@ export default function DropdownsAndInput({
                 })),
               )
         }
-        value={property || null}
+        value={state.property || null}
         groupBy={
-          !matchingEntityObjectState ? (option) => option.group : undefined
+          !state.matchingEntityObjectState
+            ? (option) => option.group
+            : undefined
         }
         isOptionEqualToValue={
-          !matchingEntityObjectState
+          !state.matchingEntityObjectState
             ? (option, value) => option.Name === value?.Name
             : undefined
         }
         sx={{
           ...AUTOCOMPLETE_STYLES,
-          ...(matchingEntityObjectState && {
+          ...(state.matchingEntityObjectState && {
             borderTopLeftRadius: 0,
             borderBottomLeftRadius: 0,
           }),
@@ -260,8 +114,10 @@ export default function DropdownsAndInput({
       />
       <Select
         name={`operator_${fieldIdentifierId}`}
-        value={operator || ''}
-        onChange={(e, newValue) => setOperator(newValue)}
+        value={state.operator || ''}
+        onChange={(e, newValue) =>
+          dispatch({ type: 'SET_OPERATOR', payload: newValue })
+        }
         required
         sx={SELECT_STYLES}
       >
@@ -275,12 +131,12 @@ export default function DropdownsAndInput({
         ))}
       </Select>
       <PropertyTypeInput
-        propertyType={property?.Type || property?.type}
+        propertyType={state.property?.Type || state.property?.type}
         name={`value_${fieldIdentifierId}`}
-        value={value}
+        value={state.value}
         onChange={handleValueChange}
         placeholder='Enter a value'
-        operator={operator}
+        operator={state.operator}
         required
         {...props}
       />
