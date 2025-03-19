09debd8b50d39668f973e563f3116391a7b0adb2
Lxkasmehl <lukasmehl007@gmail.com>
2025-03-19 09:41:25 -0500

    feat: Update FilterArea component in UiBuilder to include dynamic filtering options with Autocomplete inputs

--- src/features/uiBuilder/components/FilterArea.jsx
+++ src/features/uiBuilder/components/FilterArea.jsx
@@ -1,5 +1,19 @@
-import { Typography } from '@mui/joy';
+import { Autocomplete, FormLabel } from '@mui/joy';
 
 export default function FilterArea() {
-  return <Typography>Filter Area</Typography>;
+  return (
+    <div className='flex flex-col gap-2 flex-wrap max-h-[190px]'>
+      {[...Array(9)].map((_, index) => (
+        <div key={index} className='flex flex-col gap-1'>
+          <FormLabel size='sm'>Filter {index + 1}</FormLabel>
+          <Autocomplete
+            size='sm'
+            placeholder='Select an option'
+            options={[]}
+            sx={{ width: '90%' }}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
