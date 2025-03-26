ad0804382ad097c61e764905e3698851fc814fbd
Lxkasmehl <lukasmehl007@gmail.com>
2025-03-26 10:32:05 -0500

    feat: Add editing functionality to ChartComponent and integrate new icon in DragOverlayComponent

--- src/features/uiBuilder/components/ChartComponent.jsx
+++ src/features/uiBuilder/components/ChartComponent.jsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { LineChart, BarChart, PieChart, ScatterChart } from '@mui/x-charts';
+import { IconButton } from '@mui/joy';
+import Edit from '@mui/icons-material/Edit';
+import ChartEditModal from './ChartEditModal';
 
 const sampleData = {
   lines: {
@@ -7,7 +11,7 @@ const sampleData = {
     series: [{ data: [2, 5.5, 2, 8.5, 1.5] }],
   },
   bars: {
-    xAxis: [{ data: ['A', 'B', 'C', 'D', 'E'] }],
+    xAxis: [{ data: ['A', 'B', 'C', 'D', 'E'], scaleType: 'band' }],
     series: [{ data: [2, 5.5, 2, 8.5, 1.5] }],
   },
   pie: {
@@ -38,22 +42,96 @@ const sampleData = {
 };
 
 export default function ChartComponent({ component }) {
-  const data = sampleData[component.props.type] || sampleData.lines;
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [chartData, setChartData] = useState(
+    component.props.data ||
+      sampleData[component.props.type] ||
+      sampleData.lines,
+  );
+  const [chartType, setChartType] = useState(component.props.type);
 
-  switch (component.props.type) {
+  const handleEditClick = () => {
+    setIsEditModalOpen(true);
+  };
+
+  const handleSave = (updatedComponent) => {
+    setChartData(
+      updatedComponent.props.data ||
+        sampleData[updatedComponent.props.type] ||
+        sampleData.lines,
+    );
+    setChartType(updatedComponent.props.type);
+    setIsEditModalOpen(false);
+  };
+
+  const ChartWrapper = ({ children }) => (
+    <>
+      {children}
+      <IconButton
+        variant='solid'
+        color='primary'
+        onClick={handleEditClick}
+        sx={{
+          position: 'absolute',
+          top: '-10px',
+          right: '-10px',
+          borderRadius: '50%',
+        }}
+      >
+        <Edit />
+      </IconButton>
+      <ChartEditModal
+        open={isEditModalOpen}
+        onClose={() => setIsEditModalOpen(false)}
+        component={{
+          ...component,
+          props: { ...component.props, data: chartData, type: chartType },
+        }}
+        onSave={handleSave}
+      />
+    </>
+  );
+
+  ChartWrapper.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  switch (chartType) {
     case 'bars':
-      return <BarChart {...data} height={300} />;
+      return (
+        <ChartWrapper>
+          <BarChart {...chartData} height={300} />
+        </ChartWrapper>
+      );
     case 'lines':
-      return <LineChart {...data} height={300} />;
+      return (
+        <ChartWrapper>
+          <LineChart {...chartData} height={300} />
+        </ChartWrapper>
+      );
     case 'pie':
-      return <PieChart {...data} height={300} />;
+      return (
+        <ChartWrapper>
+          <PieChart {...chartData} height={300} />
+        </ChartWrapper>
+      );
     case 'scatter':
-      return <ScatterChart {...data} height={300} />;
+      return (
+        <ChartWrapper>
+          <ScatterChart {...chartData} height={300} />
+        </ChartWrapper>
+      );
     default:
       return null;
   }
 }
 
 ChartComponent.propTypes = {
-  component: PropTypes.object.isRequired,
+  component: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    props: PropTypes.shape({
+      type: PropTypes.string.isRequired,
+      data: PropTypes.object,
+    }).isRequired,
+  }).isRequired,
 };
