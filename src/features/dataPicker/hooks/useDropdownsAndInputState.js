5c25b0f2e8ae8bc7ffefb6f65f08811590010284
Lxkasmehl <lukasmehl007@gmail.com>
2025-03-31 20:58:05 -0500

    refactor: Update DropdownsAndInput and useDropdownsAndInputState to use local state management instead of reducer, improving clarity and performance

--- src/features/dataPicker/hooks/useDropdownsAndInputState.js
+++ src/features/dataPicker/hooks/useDropdownsAndInputState.js
@@ -1,44 +1,14 @@
-import { useReducer } from 'react';
+import { useState, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getNavigationProperties } from '../utils/entity/entityNavigation';
 import { setMatchingEntityObjects } from '../../../redux/entitiesSlice';
 import { findMatchingEntity } from '../utils/entity/entityNavigation';
 
-const initialState = {
-  property: null,
-  operator: '',
-  value: '',
-  matchingEntityObjectState: null,
-  path: '',
-  autocompleteKey: 0,
-  partialPath: '',
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'SET_PROPERTY':
-      return { ...state, property: action.payload };
-    case 'SET_OPERATOR':
-      return { ...state, operator: action.payload };
-    case 'SET_VALUE':
-      return { ...state, value: action.payload };
-    case 'SET_MATCHING_ENTITY':
-      return { ...state, matchingEntityObjectState: action.payload };
-    case 'SET_PATH':
-      return { ...state, path: action.payload };
-    case 'SET_PARTIAL_PATH':
-      return { ...state, partialPath: action.payload };
-    case 'INCREMENT_AUTOCOMPLETE_KEY':
-      return { ...state, autocompleteKey: state.autocompleteKey + 1 };
-    default:
-      return state;
-  }
-}
-
 export default function useDropdownsAndInputState(
   propertyOptionsId,
   fieldIdentifierId,
 ) {
+  // Redux selectors
   const formData = useSelector((state) => state.entities.formData);
   const allEntities = useSelector((state) => state.entities.allEntities);
   const associationSets = useSelector(
@@ -53,40 +23,18 @@ export default function useDropdownsAndInputState(
   const selectedEntities = useSelector(
     (state) => state.entities.selectedEntities,
   );
-  const selectedEntity = selectedEntities[propertyOptionsId];
-  // const { combinedOptions } = usePropertyOptions(propertyOptionsId);
   const propertyOptions = useSelector(
     (state) => state.entities.propertyOptions[propertyOptionsId],
   );
   const dispatch = useDispatch();
 
-  const baseFieldId = fieldIdentifierId.includes('group_')
-    ? fieldIdentifierId.split('group_')[1].split('_').slice(1).join('_')
-    : fieldIdentifierId;
-
-  const [state, localDispatch] = useReducer(reducer, {
-    ...initialState,
-    ...getInitialPropertyState(),
-    operator:
-      formData[propertyOptionsId]?.[`operator_${fieldIdentifierId}`] ?? '',
-    value: formData[propertyOptionsId]?.[`value_${fieldIdentifierId}`] ?? '',
-    path: formData[propertyOptionsId]?.[`fullPath_${fieldIdentifierId}`] ?? '',
-    matchingEntityObjectState:
-      matchingEntityObjects[propertyOptionsId]?.[baseFieldId] ??
-      matchingEntityObjects[propertyOptionsId]?.[fieldIdentifierId],
-    partialPath: (() => {
-      const initialPath =
-        formData[propertyOptionsId]?.[`fullPath_${fieldIdentifierId}`] ?? '';
-      const lastSlashIndex = initialPath.lastIndexOf('/');
-      return lastSlashIndex >= 0
-        ? initialPath.substring(0, lastSlashIndex)
-        : '';
-    })(),
-  });
-
-  function getInitialPropertyState() {
+  // Local state
+  const [property, setProperty] = useState(() => {
     const propertyFromForm =
       formData[propertyOptionsId]?.[`property_${fieldIdentifierId}`];
+    const baseFieldId = fieldIdentifierId.includes('group_')
+      ? fieldIdentifierId.split('group_')[1].split('_').slice(1).join('_')
+      : fieldIdentifierId;
 
     const matchingProperty = matchingEntityObjects[propertyOptionsId]?.[
       baseFieldId
@@ -108,125 +56,127 @@ export default function useDropdownsAndInputState(
             null
           : null;
 
-    return {
-      property: matchingProperty,
-    };
-  }
+    return matchingProperty;
+  });
 
-  const handleValueChange = (e) => {
-    localDispatch({
-      type: 'SET_VALUE',
-      payload: typeof e === 'object' && e.target ? e.target.value : e,
-    });
-  };
-
-  const handlePropertyChange = (e, newValue) => {
-    if (!newValue) return;
-
-    const newValueName = newValue.Name;
-    const currentEntity =
-      state.matchingEntityObjectState?.matchingEntity ||
-      filteredEntities.find((e) => e.name === selectedEntity);
-
-    const navigationProperties = getNavigationProperties(currentEntity);
-
-    const isNavigationProperty = navigationProperties.some(
-      (np) => np.Name === newValueName,
+  const [operator, setOperator] = useState(
+    formData[propertyOptionsId]?.[`operator_${fieldIdentifierId}`] ?? '',
+  );
+  const [value, setValue] = useState(
+    formData[propertyOptionsId]?.[`value_${fieldIdentifierId}`] ?? '',
+  );
+  const [path, setPath] = useState(
+    formData[propertyOptionsId]?.[`fullPath_${fieldIdentifierId}`] ?? '',
+  );
+  const [partialPath, setPartialPath] = useState(() => {
+    const initialPath =
+      formData[propertyOptionsId]?.[`fullPath_${fieldIdentifierId}`] ?? '';
+    const lastSlashIndex = initialPath.lastIndexOf('/');
+    return lastSlashIndex >= 0 ? initialPath.substring(0, lastSlashIndex) : '';
+  });
+  const [matchingEntityState, setMatchingEntityState] = useState(() => {
+    const baseFieldId = fieldIdentifierId.includes('group_')
+      ? fieldIdentifierId.split('group_')[1].split('_').slice(1).join('_')
+      : fieldIdentifierId;
+    return (
+      matchingEntityObjects[propertyOptionsId]?.[baseFieldId] ??
+      matchingEntityObjects[propertyOptionsId]?.[fieldIdentifierId]
     );
+  });
+  const [autocompleteKey, setAutocompleteKey] = useState(0);
 
-    if (!isNavigationProperty) {
-      localDispatch({ type: 'SET_PROPERTY', payload: newValue });
-      const pathParts = state.path.split('/');
+  const handleValueChange = useCallback((e) => {
+    setValue(typeof e === 'object' && e.target ? e.target.value : e);
+  }, []);
 
-      let relevantNavigationProperties = navigationProperties;
-      if (pathParts.length > 0) {
-        let currentEntity = filteredEntities.find(
-          (e) => e.name === selectedEntity,
-        );
+  const handlePropertyChange = useCallback(
+    (e, newValue) => {
+      if (!newValue) return;
 
-        let newCurrentEntity = currentEntity;
-        if (pathParts.length > 1) {
-          const result = findMatchingEntity({
-            propertyName: pathParts[pathParts.length - 1],
-            navigationProperties: currentEntity.properties.navigationProperties,
-            associationSets,
-            allEntities,
-          });
-          newCurrentEntity = result?.matchingEntity;
-        }
-
-        if (newCurrentEntity) {
-          relevantNavigationProperties =
-            newCurrentEntity.properties.navigationProperties;
-        }
-      }
-
-      const lastPartIsNavigation = relevantNavigationProperties.some(
-        (np) => np.Name === pathParts[pathParts.length - 1],
+      const newValueName = newValue.Name;
+      const selectedEntity = selectedEntities[propertyOptionsId];
+      const currentEntity =
+        matchingEntityState?.matchingEntity ||
+        filteredEntities.find((e) => e.name === selectedEntity);
+      const navigationProperties = getNavigationProperties(currentEntity);
+      const isNavigationProperty = navigationProperties.some(
+        (np) => np.Name === newValueName,
       );
 
-      const newPath = state.matchingEntityObjectState
-        ? pathParts.length > 0
-          ? lastPartIsNavigation
-            ? `${state.path}/${newValueName}`
-            : [...pathParts.slice(0, -1), newValueName].join('/')
-          : newValueName
-        : newValueName;
+      if (!isNavigationProperty) {
+        setProperty(newValue);
+        const pathParts = path.split('/');
+        const newPath =
+          pathParts.length > 0
+            ? `${pathParts.slice(0, -1).join('/')}/${newValueName}`
+            : newValueName;
 
-      localDispatch({ type: 'SET_PATH', payload: newPath });
-      localDispatch({
-        type: 'SET_MATCHING_ENTITY',
-        payload: {
-          ...state.matchingEntityObjectState,
-          path: newPath,
-        },
-      });
-      localDispatch({ type: 'INCREMENT_AUTOCOMPLETE_KEY' });
-      return;
-    }
+        setPath(newPath);
+        setMatchingEntityState((prev) => ({ ...prev, path: newPath }));
+        setAutocompleteKey((prev) => prev + 1);
+        return;
+      }
 
-    const newPath = state.matchingEntityObjectState
-      ? `${state.partialPath !== '' ? `${state.partialPath}/` : ''}${newValueName}`
-      : newValueName;
-    localDispatch({ type: 'SET_PATH', payload: newPath });
-    localDispatch({ type: 'SET_PARTIAL_PATH', payload: newPath });
+      const newPath = `${partialPath !== '' ? `${partialPath}/` : ''}${newValueName}`;
+      setPath(newPath);
+      setPartialPath(newPath);
+      setProperty(null);
 
-    const { matchingEntity } =
-      findMatchingEntity({
-        propertyName: newValueName,
-        navigationProperties,
-        associationSets,
-        allEntities,
-      }) || {};
+      const { matchingEntity } =
+        findMatchingEntity({
+          propertyName: newValueName,
+          navigationProperties,
+          associationSets,
+          allEntities,
+        }) || {};
 
-    const matchingEntityObject = matchingEntity
-      ? { path: newPath, matchingEntity }
-      : { path: newPath, matchingEntity: {} };
+      const newMatchingEntityState = matchingEntity
+        ? { path: newPath, matchingEntity }
+        : { path: newPath, matchingEntity: {} };
 
-    localDispatch({
-      type: 'SET_MATCHING_ENTITY',
-      payload: matchingEntityObject,
-    });
-    localDispatch({ type: 'INCREMENT_AUTOCOMPLETE_KEY' });
-    localDispatch({ type: 'SET_PROPERTY', payload: null });
+      setMatchingEntityState(newMatchingEntityState);
+      setAutocompleteKey((prev) => prev + 1);
 
-    const updatedMatchingEntityObjects = {
-      ...matchingEntityObjects[propertyOptionsId],
-      [fieldIdentifierId]: matchingEntity,
-    };
+      // Update Redux state
+      const updatedMatchingEntityObjects = {
+        ...matchingEntityObjects[propertyOptionsId],
+        [fieldIdentifierId]: matchingEntity,
+      };
 
-    dispatch(
-      setMatchingEntityObjects({
-        id: propertyOptionsId,
-        matchingEntityObjects: updatedMatchingEntityObjects,
-      }),
-    );
-  };
+      dispatch(
+        setMatchingEntityObjects({
+          id: propertyOptionsId,
+          matchingEntityObjects: updatedMatchingEntityObjects,
+        }),
+      );
+    },
+    [
+      path,
+      partialPath,
+      matchingEntityState,
+      propertyOptionsId,
+      fieldIdentifierId,
+      selectedEntities,
+      filteredEntities,
+      matchingEntityObjects,
+      associationSets,
+      allEntities,
+      dispatch,
+    ],
+  );
 
   return {
-    state,
+    state: {
+      property,
+      operator,
+      value,
+      path,
+      partialPath,
+      matchingEntityState,
+      autocompleteKey,
+    },
     handleValueChange,
     handlePropertyChange,
-    localDispatch,
+    setOperator,
   };
 }
