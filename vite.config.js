98d104620ad5bf1f9201bc82969c88f8be044ebc
Lxkasmehl <lukasmehl007@gmail.com>
2025-07-03 16:52:50 +0200

    chore: update .gitignore to include key.pem, cert.pem, and dist.zip; configure Vite server for HTTPS using provided certificates

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -26,6 +27,10 @@ export default defineConfig({
     },
   },
   server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+    },
     proxy: {
       '/api': {
         target: 'https://api5.successfactors.eu',
