2c371581599db03997f2ae55bf130801a090cf27
Lxkasmehl <lukasmehl007@gmail.com>
2025-09-01 13:58:15 -0500

    integrate Joy UI theme provider in main.jsx; enhance TableComponent with MUI CSS variables provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,39 @@
-// src/index.js
+// src/index.tsx|js
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { StrictMode } from 'react';
-import store from './redux/store';
+import { Provider } from 'react-redux';
 import { ReactFlowProvider } from '@xyflow/react';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/index';
+import store from './redux/store';
+import { router } from './routes';
+import {
+  CssVarsProvider as JoyCssVarsProvider,
+  extendTheme,
+} from '@mui/joy/styles';
 
-ReactDOM.createRoot(document.getElementById('pidgetReactContainer')).render(
-  <ReactFlowProvider>
-    <StrictMode>
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
-    </StrictMode>
-  </ReactFlowProvider>,
-);
+const joyTheme = extendTheme({
+  components: {
+    JoyAutocomplete: {
+      styleOverrides: {
+        root: {
+          inset: '0px auto auto 0px !important',
+        },
+      },
+    },
+  },
+});
+
+const container = document.getElementById('pidgetReactContainer');
+if (container) {
+  ReactDOM.createRoot(container).render(
+    <JoyCssVarsProvider theme={joyTheme}>
+      <ReactFlowProvider>
+        <StrictMode>
+          <Provider store={store}>
+            <RouterProvider router={router} />
+          </Provider>
+        </StrictMode>
+      </ReactFlowProvider>
+    </JoyCssVarsProvider>,
+  );
+}
