9333eec4a27cb86119792eccb42e05d090a170c9
Lxkasmehl <lukasmehl007@gmail.com>
2025-09-11 19:55:32 -0500

    refactor: important inset style in index.css commented out due to malfunction and tests simplified again

--- e2e/features/datapicker/tests/filterFlow.spec.js
+++ e2e/features/datapicker/tests/filterFlow.spec.js
@@ -98,11 +98,6 @@ test('use selected properties from one entity section as filter values in anothe
     Granger: 2, 
   };
 
-  await page.screenshot({
-    path: 'debug-candidate-selection.png',
-    fullPage: true,
-  });
-
   for (const [name, count] of Object.entries(expectedNames)) {
     const elements = await page.locator(`text=lastName: ${name}`).all();
     expect(elements).toHaveLength(count);
