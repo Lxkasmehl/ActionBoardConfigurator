f8bc210b676f01b7e0180c00588fbbf9fdb5434a
Lxkasmehl <lukasmehl007@gmail.com>
2025-09-11 20:16:08 -0500

    Revert "refactor: important inset style in index.css commented out due to malfunction and tests simplified again"
    This reverts commit 9333eec4a27cb86119792eccb42e05d090a170c9.

--- e2e/features/uibuilder/helpers/tableHelpers.js
+++ e2e/features/uibuilder/helpers/tableHelpers.js
@@ -1,6 +1,9 @@
 import { expect } from '@playwright/test';
 import { dragAndVerifyComponent } from './componentHelpers';
-import { selectFromAutocomplete } from '../../../shared/helpers/autocompleteHelper';
+import {
+  selectFromAutocomplete,
+  selectFromEntitySelect,
+} from '../../../shared/helpers/autocompleteHelper';
 import { setupFlowConnection } from '../../datapicker/helpers/flowSetup';
 import fs from 'fs';
 
@@ -243,7 +246,7 @@ export async function configureTableColumn(
     await selectFromAutocomplete(page, 'entity-autocomplete', entity);
     await selectFromAutocomplete(page, 'property-selector', property);
   } else {
-    await selectFromAutocomplete(page, 'entity-select', entity, 0, {
+    await selectFromEntitySelect(page, 'entity-select', entity, 0, {
       useSection: false,
     });
 
@@ -270,7 +273,7 @@ export async function configureTableColumn(
       );
     }
 
-    await selectFromAutocomplete(page, 'property-select', property, 0, {
+    await selectFromEntitySelect(page, 'property-select', property, 0, {
       useSection: false,
     });
   }
