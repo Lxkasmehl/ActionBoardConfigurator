78f683462f57a26c15a4861a9e45fa8a7c9b65d4
Lxkasmehl <lukasmehl007@gmail.com>
2025-09-27 17:56:18 -0500

    chore: remove deprecated src folder

--- src/features/dataPicker/components/PropertyTypeInput.constants.js
+++
@@ -1,5 +0,0 @@
-export const COMMON_INPUT_STYLES = {
-  borderRadius: 0,
-  width: 220,
-  height: '36px',
-};
--- src/redux/selectors/entitySelectors.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-export const selectPropertyOptions = createSelector(
-  (state) => state.dataPicker.propertyOptions,
-  (_, id) => id,
-  (propertyOptions, id) => propertyOptions[id] || [],
-);
